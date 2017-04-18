49377462941beec2e12e07312d8eb7da5e7b4c4c
Jason Gilanfarr <jgilanfarr@mesosphere.com>
2017-04-17 19:12:08 -0700

    Try to find out where the packageworks

--- scripts/publish_packages.sh
+++ scripts/publish_packages.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-env
-
 
 cd repo/incoming/marathon-${GIT_TAG}
+
+ls $HOME
+ls $HOME/bin
 
 PKG_TYPE=""
 case "$GIT_TAG" in
@@ -18,26 +19,26 @@ echo "Publishing marathon with ${GIT_TAG} ($(ls *)) to ${PKG_TYPE}"
 
 
 # Debian 8
-cp systemd-marathon*.deb ../incoming/debian/jessie${PKG_TYPE}/
+cp systemd-marathon*.deb $HOME/repo/incoming/debian/jessie${PKG_TYPE}/
 # Debian 9
-cp systemd-marathon*.deb ../incoming/debian/stretch${PKG_TYPE}/
+cp systemd-marathon*.deb $HOME/repo/incoming/debian/stretch${PKG_TYPE}/
 
 
 # Ubuntu 16.04
-cp systemd-marathon*.deb ../incoming/ubuntu/yakkety${PKG_TYPE}/
+cp systemd-marathon*.deb $HOME/repo/incoming/ubuntu/yakkety${PKG_TYPE}/
 # Ubuntu 16.10
-cp systemd-marathon*.deb ../incoming/ubuntu/xenial${PKG_TYPE}/
+cp systemd-marathon*.deb $HOME/repo/incoming/ubuntu/xenial${PKG_TYPE}/
 # Ubuntu 15.10
-cp systemd-marathon*.deb ../incoming/ubuntu/wily${PKG_TYPE}/
+cp systemd-marathon*.deb $HOME/repo/incoming/ubuntu/wily${PKG_TYPE}/
 # Ubuntu 15.04
-cp systemd-marathon*.deb ../incoming/ubuntu/vivid${PKG_TYPE}/
+cp systemd-marathon*.deb $HOME/repo/incoming/ubuntu/vivid${PKG_TYPE}/
 # Ubuntu 14.04
-cp upstart-marathon*.deb ../incoming/ubuntu/trusty${PKG_TYPE}/
+cp upstart-marathon*.deb $HOME/repo/incoming/ubuntu/trusty${PKG_TYPE}/
 # Ubuntu 12.04
-cp upstart-marathon*.deb ../incoming/ubuntu/precide${PKG_TYPE}/
+cp upstart-marathon*.deb $HOME/repo/incoming/ubuntu/precide${PKG_TYPE}/
 
 # CentOS6
-cp sysvinit-marathon*.rpm ../incoming/el${PKG_TYPE}/6/
+cp sysvinit-marathon*.rpm $HOME/repo/incoming/el${PKG_TYPE}/6/
 
 # CentOS 7
-cp systemd-marathon*.rpm ../incoming/el${PKG_TYPE}/7/
+cp systemd-marathon*.rpm $HOME/repo/incoming/el${PKG_TYPE}/7/
