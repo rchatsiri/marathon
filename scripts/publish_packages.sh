f56631a6023ce5e452fd8d9e344bd27922c4c22c
Jason Gilanfarr <jgilanfarr@mesosphere.com>
2017-04-17 19:17:26 -0700

    Keep discovering

--- scripts/publish_packages.sh
+++ scripts/publish_packages.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 cd repo/incoming/marathon-${GIT_TAG}
 
 ls $HOME
-ls $HOME/bin
+ls $HOME/repo
 
 PKG_TYPE=""
 case "$GIT_TAG" in
@@ -21,7 +22,8 @@ echo "Publishing marathon with ${GIT_TAG} ($(ls *)) to ${PKG_TYPE}"
 # Debian 8
 cp systemd-marathon*.deb $HOME/repo/incoming/debian/jessie${PKG_TYPE}/
 # Debian 9
-cp systemd-marathon*.deb $HOME/repo/incoming/debian/stretch${PKG_TYPE}/
+# we don't actually use stretch yet
+# cp systemd-marathon*.deb $HOME/repo/incoming/debian/stretch${PKG_TYPE}/
 
 
 # Ubuntu 16.04
@@ -35,7 +37,7 @@ cp systemd-marathon*.deb $HOME/repo/incoming/ubuntu/vivid${PKG_TYPE}/
 # Ubuntu 14.04
 cp upstart-marathon*.deb $HOME/repo/incoming/ubuntu/trusty${PKG_TYPE}/
 # Ubuntu 12.04
-cp upstart-marathon*.deb $HOME/repo/incoming/ubuntu/precide${PKG_TYPE}/
+cp upstart-marathon*.deb $HOME/repo/incoming/ubuntu/precise${PKG_TYPE}/
 
 # CentOS6
 cp sysvinit-marathon*.rpm $HOME/repo/incoming/el${PKG_TYPE}/6/
